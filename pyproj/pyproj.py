0ac3620e2631b579e8cad447cbaa7070b2e0c806
Alejandro Cabrera <alejandro.cabrera@rackspace.com>
2013-01-19 14:13:45 -0500

    fix(factorial): docstring indentation.

--- pyproj/pyproj.py
+++ pyproj/pyproj.py
@@ -1,11 +1,11 @@
 def factorial(n):
-""" Computes the factorial of an integer.
-Type:
-  factorial :: Number -> Number
+    """ Computes the factorial of an integer.
+    Type:
+      factorial :: Number -> Number
 
-Raises:
-  TypeError: n is not an integer
-"""
+    Raises:
+      TypeError: n is not an integer
+    """
     res = 1
     for i in range(1, n+1):
         res *= i
